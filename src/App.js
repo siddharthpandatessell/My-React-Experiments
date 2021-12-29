946fa9ed3a77a4d181155436ed35d5c368e5d0a1
Siddharth <sidtessel@Siddharths-MacBook-Pro.local>
2021-12-29 15:01:00 +0530

    Added the CSS grid layout for the UI, currently mui doesn't have css grids

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import ClassicResponsive from './Components/Layouts/ClassicResponsive';
+// import ClassicResponsive from './Components/Layouts/ClassicResponsive';
+import GridResponsive from './Components/Layouts/GridResponsive'
 
 function App() {
     return ( 
     <div className = "App">
-        <ClassicResponsive></ClassicResponsive>
+        {/* <ClassicResponsive></ClassicResponsive> */}
+        <GridResponsive></GridResponsive>
         </div>
     );
 }
---
+++ src/Components/Layouts/GridResponsive/index.js
@@ -0,0 +1,22 @@
+import './grid.css';
+
+function index() {
+    return ( 
+    <div className = "App">
+        <header>
+  Header
+</header>
+<aside>
+  Sidebar
+</aside>
+<main>
+  Main
+</main>
+<footer>
+  Footer
+</footer>
+        </div>
+    );
+}
+
+export default index;
