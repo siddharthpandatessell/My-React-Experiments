090acfff5321bb60f94aa10fe91b4e40c286cf6c
Siddharth <sidtessel@Siddharths-MacBook-Pro.local>
2021-12-29 16:03:31 +0530

    Added code to work with mui flexbox layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-// import ClassicResponsive from './Components/Layouts/ClassicResponsive';
+import ClassicResponsive from './Components/Layouts/ClassicResponsive';
 import GridResponsive from './Components/Layouts/GridResponsive'
 
 function App() {
     return ( 
     <div className = "App">
-        {/* <ClassicResponsive></ClassicResponsive> */}
-        <GridResponsive></GridResponsive>
+        <ClassicResponsive></ClassicResponsive>
+        {/* <GridResponsive></GridResponsive> */}
         </div>
     );
 }
--- src/Components/Layouts/ClassicResponsive.js
+++ src/Components/Layouts/ClassicResponsive.js
@@ -3,11 +3,12 @@ import Footer from "../Sections/Footer";
 import Content from "../Sections/Content";
 import Sidebar from "../Sections/SideBar";
 import Box from '@mui/material/Box'
+import Navbar from '../Elements/Navigation/NavigationBar/Classic/';
 
 function ClassicResponsive() {
     return ( 
     <div className = "App">
-        <Header content='Shimotakatani-kun'></Header>
+        <Navbar />
         <Box display="flex" flexDirection="row" width="100%" alignItems="center">
         <Sidebar></Sidebar>
         <Content></Content>
